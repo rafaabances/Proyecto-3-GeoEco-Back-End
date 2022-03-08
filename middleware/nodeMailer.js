8d00a92ff8386fe189c72950095a63de4e247383
Rafa95zgz <rafaelabancesserrate@gmail.com>
2022-03-08 17:31:44 +0100

    Correo y Readme finalizado

--- middleware/nodeMailer.js
+++ middleware/nodeMailer.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const userMail = process.env.USER_MAIL
 const pass = process.env.PASS_MAIL
 
-const transport = nodemailer.createTransport({    // Con esto creo la conexión SMTP y le paso las credenciales del correo remitente
-    service: "Gmail",
+const transport = nodemailer.createTransport({    // Con esto creo la conexión SMTP (tipo de conexion) y le paso las credenciales del correo remitente
+    service: "Gmail", // se puede aplicar a hotmail
     pool: true,
-    host: 'smtp.gmail.com', // Gmail as mail client
+    host: 'smtp.gmail.com', // Gmail as mail client / si fuera hotmail pondrías smtp.hotmail.com
     port: 465,
     secure: true, // use SSL
 
@@ -23,11 +23,11 @@ const transport = nodemailer.createTransport({    // Con esto creo la conexión 
 // Aquí creo el correo de bienvenida que llamaré la función una vez creado el usuario
 
 module.exports.sendWelcomeEmail = (email, password, name) => {
-  console.log("Check");
-  transport.sendMail({
+//   console.log("Check");
+  transport.sendMail({ // transpor es una dependencia de nodemailer y sendmail y sendWelcomeEmail sopn funciones (le ponemos el nombre que queramos)
     from: userMail,
     to: email,
-    subject: "¡Bienvenido a .... nombre de tu app .... !",
+    subject: "¡Bienvenido a GeoEco!",
     html: `<h1>¡Datos de acceso a la aplicación!</h1>
         <h2>¡¡Hola ${name}!! </h2>
         <p>Tus datos de acceso son los siguientes:</p>
